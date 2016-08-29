58311ca6b8142d58c12294d2880f26c368acaa9d
fcffc <Fernanda@Fernanda>
2016-08-28 21:53:27 -0400

    Converte String para Inteiro p/ total de Ingredientes cadastrados.

--- src/main/webapp/static/js/ingredientes.js
+++ src/main/webapp/static/js/ingredientes.js
@@ -27,12 +27,13 @@ var aplicarListeners = function(){
 	
 	$('.btn-deletar').on('click', function(){
 		var id = $(this).parents('tr').data('id');
-		var ingredientes = $('#quantidade-ingredientes').text();
+		
 		$.ajax({
 			url: "ingredientes/"+id,
 			type: 'DELETE',
 			success: function(result){
 				$('tr[data-id="'+id+'"]').remove();
+				var ingredientes = parseInt($('#quantidade-ingredientes').text());
 				$('#quantidade-ingredientes').text(ingredientes - 1);
 			}
 		});
